0c59da73504700a01376d7ef076622c825e56817
LeandraInacio <leandrainaciodepaula@gmail.com>
2021-10-04 15:57:34 -0300

    Estruturas de repetição parte 3

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,15 @@
+package repeticaofor;
+
+public class RepeticaoFor {
+
+    public static void main(String[] args) {
+        
+        for (int i=1; i<=3; i++){
+            for(int j=0; j<=2; j++){
+                System.out.println("I: "+i+" J: "+j);
+            }
+            
+        }
+    }
+    
+}
